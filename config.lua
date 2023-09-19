893c3b468f81a019f438053bf2917103e57c83f8
Alex Ragalie <2127095+aragalie@users.noreply.github.com>
2023-09-19 12:14:30 +0200

    customize

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 -- Neovim
 -- =========================================
-lvim.leader = " "
+lvim.leader = ";"
 lvim.colorscheme = "tokyonight" -- set to a custom theme
 lvim.builtin.time_based_themes = true -- set false to use your own configured theme
 lvim.transparent_window = false -- enable/disable transparency
@@ -76,7 +76,7 @@ lvim.builtin.mind = { active = false, root_path = "~/.mind" } -- enable/disable 
 -- Custom User Config
 -- =========================================
 local user = vim.env.USER
-if user and user == "abz" then
+if user and user == "alex" then
   lvim.reload_config_on_save = true
   require("user.custom_user").config()
 end
